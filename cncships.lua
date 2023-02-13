af07f5407cbf78362771d00d69e6904df6e4a2ca
ChronoVortex <nick@hudsucker.com>
2023-02-12 21:07:41 -0800

    fixed starting event compatability, added error for missing vertex

--- cncships.lua
+++ cncships.lua
@@ -1,3 +1,7 @@
+if not mods or not mods.vertexutil then
+    error("Couldn't find Vertex Tags and Utility Functions! Make sure it's above mods which depend on it in the Slipstream load order", 2)
+end
+
 local vter = mods.vertexutil.vter
 local ShowTutorialArrow = mods.vertexutil.ShowTutorialArrow
 local HideTutorialArrow = mods.vertexutil.HideTutorialArrow
