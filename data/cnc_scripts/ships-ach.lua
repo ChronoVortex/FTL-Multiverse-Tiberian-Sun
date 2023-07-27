5206dcff54b496f1172fc50db7593f67c9e921da
ChronoVortex <nick@hudsucker.com>
2023-07-27 06:59:53 -0700

    completed banshee achievements

--- data/cnc_scripts/ships-ach.lua
+++ data/cnc_scripts/ships-ach.lua
@@ -3,6 +3,8 @@
 ----------------------
 
 local vter = mods.vertexutil.vter
+local is_first_shot = mods.vertexutil.is_first_shot
+local userdata_table = mods.vertexutil.userdata_table
 
 local function string_starts(str, start)
     return string.sub(str, 1, string.len(start)) == start
@@ -93,10 +95,91 @@ do
     end)
 end
 
--- C layout unlock
+-------------
+-- BANSHEE --
+-------------
+
+-- Easy
+local cloakShotCount = 0
+local function is_ship_cloaked(ship)
+    return ship.cloakSystem and ship.cloakSystem.bTurnedOn
+end
+script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
+    if ship.iShipId == 0 and not is_ship_cloaked(ship) then
+        cloakShotCount = 0
+    end
+end)
+script.on_internal_event(Defines.InternalEvents.PROJECTILE_FIRE, function(projectile, weapon)
+    local playerShip = Hyperspace.ships.player
+    local checkCloakShot = weapon.iShipId == 0 and
+                           is_ship_cloaked(playerShip) and
+                           playerShip:HasAugmentation("CLOAK_FIRE") <= 0 and
+                           playerShip:GetAugmentationValue("CLOAK_FIRE") <= 0 and
+                           (weapon.blueprint.typeName == "BEAM" or is_first_shot(weapon, true)) and
+                           should_track_achievement("ACH_SHIP_BANSHEE_1", playerShip, "PLAYER_SHIP_BANSHEE")
+    if checkCloakShot then
+        cloakShotCount = cloakShotCount + 1
+        if cloakShotCount >= 10 then
+            Hyperspace.CustomAchievementTracker.instance:SetAchievement("ACH_SHIP_BANSHEE_1", false)
+        end
+    end
+end)
+
+-- Normal
+script.on_internal_event(Defines.InternalEvents.ON_TICK, function()
+    if should_track_achievement("ACH_SHIP_BANSHEE_2", Hyperspace.ships.player, "PLAYER_SHIP_BANSHEE") then
+        local playerVars = Hyperspace.playerVariables
+        local metaVars = Hyperspace.metaVariables
+        local generalRep = playerVars.rep_general
+        if generalRep > playerVars.loc_rep_general_last then
+            playerVars.loc_banshee_evil_score = playerVars.loc_banshee_evil_score + generalRep - playerVars.loc_rep_general_last
+        end
+        if metaVars.prof_finalsector > playerVars.loc_finalsector_last then
+            if playerVars.loc_banshee_evil_score >= 4 and generalRep <= 0 then
+                Hyperspace.CustomAchievementTracker.instance:SetAchievement("ACH_SHIP_BANSHEE_2", false)
+            end
+        end
+        playerVars.loc_finalsector_last = metaVars.prof_finalsector
+        playerVars.loc_rep_general_last = generalRep
+    end
+end)
+
+-- Hard
+script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
+    local shipAchData = userdata_table(ship, "mods.cnc.achTracking")
+    if ship.iShipId == 1 and not shipAchData.systemFiresCounted and should_track_achievement("ACH_SHIP_BANSHEE_3", Hyperspace.ships.player, "PLAYER_SHIP_BANSHEE") then
+        for system in vter(ship.vSystemList) do
+            if ship:HasSystem(system:GetId()) and not system.bOnFire then return end
+        end
+        shipAchData.systemFiresCounted = true
+        Hyperspace.playerVariables.loc_banshee_fire_score = Hyperspace.playerVariables.loc_banshee_fire_score + 1
+        print(Hyperspace.playerVariables.loc_banshee_fire_score)
+        if Hyperspace.playerVariables.loc_banshee_fire_score >= 4 then
+            Hyperspace.CustomAchievementTracker.instance:SetAchievement("ACH_SHIP_BANSHEE_3", false)
+        end
+    end
+end)
+
+-------------------------------------
+-- LAYOUT UNLOCKS FOR ACHIEVEMENTS --
+-------------------------------------
+
+local achLayoutUnlocks = {
+    {
+        achPrefix = "ACH_SHIP_KODIAK",
+        unlockShip = "PLAYER_SHIP_KODIAK_3"
+    },
+    {
+        achPrefix = "ACH_SHIP_BANSHEE",
+        unlockShip = "PLAYER_SHIP_BANSHEE_3"
+    }
+}
+
 script.on_internal_event(Defines.InternalEvents.ON_TICK, function()
     local unlockTracker = Hyperspace.CustomShipUnlocks.instance
-    if not unlockTracker:GetCustomShipUnlocked("PLAYER_SHIP_KODIAK_3") and count_ship_achievements("ACH_SHIP_KODIAK") >= 2 then
-        unlockTracker:UnlockShip("PLAYER_SHIP_KODIAK_3", false)
+    for _, unlockData in ipairs(achLayoutUnlocks) do
+        if not unlockTracker:GetCustomShipUnlocked(unlockData.unlockShip) and count_ship_achievements(unlockData.achPrefix) >= 2 then
+            unlockTracker:UnlockShip(unlockData.unlockShip, false)
+        end
     end
 end)
